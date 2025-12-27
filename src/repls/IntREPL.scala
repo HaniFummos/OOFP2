df57eecb996e14e0ad936b86f6bbe228fa3704a1
Hani Al-Razzaz <Hani.Al-Razzaz@hotmail.com>
2025-12-27 18:19:29 +0100

    IntREPL complete

--- src/repls/IntREPL.scala
+++ src/repls/IntREPL.scala
@@ -1,8 +1,13 @@
 package repls
 
+import scala.collection.mutable
+
 /*
-* todo: first simply do shunting yard, then understand all nuances about class structure & pattern matching,
-*  then see where it goes and understand any more if needed...
+* todo:
+*  -Fix the 3 simplify parts to pass advanced & distributivity.
+*  -Fix pretty printing
+*  -Do the multiset operations
+*  -Later... Do multisetREPL
 * */
 //test
 class IntREPL extends REPLBase {
@@ -18,7 +23,8 @@ class IntREPL extends REPLBase {
         tokens.head match {
             case "@" =>
                 val expr = parseExpression(tokens.tail) //remove the @
-                simplify(expr)
+                val simplified = simplify(expr, currentBindings)  // Returns Expression
+                prettyPrint(simplified)
             case variable if tokens.length >= 3 && tokens(1) == "=" => // variable = tokens.head
                 val expr = parseExpression(tokens.drop(2)) //drop(2) takes tokens without the first 2, removing the varname and equals
                 assign(variable, expr)
@@ -29,22 +35,22 @@ class IntREPL extends REPLBase {
     }
 
     abstract class Expression { //abstract = no function. We use it so that simplify() later on has a uniform type to work with for all const, var, add, mul, sub
-        def eval(bindings : Map[String,Int]) : Int //For separation of concerns we do not use currentBindings...
+        def value(bindings : Map[String,Int]) : Int //For separation of concerns & modularity we do not use currentBindings :)
     }
     case class Const(i : Int) extends Expression {
-        override def eval(bindings: Map[String, Int]): Int = i //no storage, just return the value
+        override def value(bindings: Map[String, Int]): Int = i //no storage, just return the value
     }
     case class Var(name: String) extends Expression {
-        override def eval(bindings: Map[String, Int]): Int = bindings(name) //returns the value associated with our varname from the provided mapping
+        override def value(bindings: Map[String, Int]): Int = bindings(name) //returns the value associated with our varname from the provided mapping
     }
     case class Add(left: Expression, right: Expression) extends Expression {
-        override def eval(bindings: Map[String, Int]): Int = left.eval(bindings) + right.eval(bindings) //evaluate LHS and RHS (recursion), then add
+        override def value(bindings: Map[String, Int]): Int = left.value(bindings) + right.value(bindings) //evaluate LHS and RHS (recursion), then add
     }
     case class Mul(left: Expression, right: Expression) extends Expression {
-        override def eval(bindings: Map[String, Int]): Int = left.eval(bindings) * right.eval(bindings) //evaluate LHS and RHS (recursion), then mul
+        override def value(bindings: Map[String, Int]): Int = left.value(bindings) * right.value(bindings) //evaluate LHS and RHS (recursion), then mul
     }
     case class Sub(left: Expression, right: Expression) extends Expression {
-        override def eval(bindings: Map[String, Int]): Int = left.eval(bindings) - right.eval(bindings) //evaluate LHS and RHS (recursion), then sub
+        override def value(bindings: Map[String, Int]): Int = left.value(bindings) - right.value(bindings) //evaluate LHS and RHS (recursion), then sub
     }
 
     private def parseExpression(tokens: Seq[String]): Expression = {
@@ -57,8 +63,8 @@ class IntREPL extends REPLBase {
         case _ => 0
     }
     private def shuntingYard(tokens: Seq[String]): Seq[String] = {
-        val output = collection.mutable.Queue[String]()
-        val stack = collection.mutable.Stack[String]()
+        val output = mutable.Queue[String]()
+        val stack = mutable.Stack[String]()
 
         for (token <- tokens) {
             token match {
@@ -86,24 +92,152 @@ class IntREPL extends REPLBase {
         output.toSeq
     }
 
-    private def buildExpressionTree(rpn: Seq[String]): Expression = {
+    def isNumber(s: String): Boolean = {
+        if (s.isEmpty) return false
 
-        Const(0)
+        val charsToCheck = if (s.head == '-') {
+            if (s.length == 1) return false
+            s.tail
+        }
+        else {
+            s
+        }
+
+        charsToCheck.forall(_.isDigit)
     }
 
-    private def simplify(expr: Expression): String = {
-        // TODO: Implement simplification
-        "simplified"
+    private def buildExpressionTree(rpn: Seq[String]): Expression = {
+        val stack = mutable.Stack[Expression]()
+
+        for (token <- rpn) {
+            token match {
+                case "+" =>
+                    val right = stack.pop()
+                    val left = stack.pop()
+                    stack.push(Add(left, right))
+
+                case "-" =>
+                    val right = stack.pop()
+                    val left = stack.pop()
+                    stack.push(Sub(left, right))
+
+                case "*" =>
+                    val right = stack.pop()
+                    val left = stack.pop()
+                    stack.push(Mul(left, right))
+
+                case t if isNumber(t) =>
+                    stack.push(Const(t.toInt))
+
+                case _ => //variables
+                    stack.push(Var(token))
+            }
+        }
+
+        if (stack.isEmpty) Const(0) else stack.pop()
+    }
+
+    private def simplify(expr: Expression, bindings: Map[String, Int]): Expression = { //bottom-up
+        expr match {
+            case Const(i) => Const(i)
+            case Var(name) =>
+                bindings.get(name) match {
+                    case Some(value) => Const(value)
+                    case None => Var(name)
+                }
+
+            case Add(left, right) =>
+                val sl = simplify(left, bindings)
+                val sr = simplify(right, bindings)
+                simplifyAdd(sl, sr, bindings)
+
+            case Mul(left, right) =>
+                val sl = simplify(left, bindings)
+                val sr = simplify(right, bindings)
+                simplifyMul(sl, sr)
+
+            case Sub(left, right) =>
+                val sl = simplify(left, bindings)
+                val sr = simplify(right, bindings)
+                simplifySub(sl, sr)
+        }
+    }
+
+    private def simplifyAdd(left: Expression, right: Expression, bindings: Map[String, Int]): Expression = {
+        (left, right) match {
+            case (Const(0), r) => r
+            case (l, Const(0)) => l
+            case (Const(a), Const(b)) => Const(a + b) //easy cases...
+
+            case (Mul(a1, b), Mul(a2, c)) if a1 == a2 => Mul(simplify(a1, bindings), simplify(Add(b, c), bindings)) //(even though we do bottom up, we will guaranteed catch these patterns eventually)
+            case (Mul(b, a1), Mul(a2, c)) if a1 == a2 => Mul(simplify(a1, bindings), simplify(Add(b, c), bindings))
+            case (Mul(a1, b), Mul(c, a2)) if a1 == a2 => Mul(simplify(a1, bindings), simplify(Add(b, c), bindings))
+            case (Mul(b, a1), Mul(c, a2)) if a1 == a2 => Mul(simplify(a1, bindings), simplify(Add(b, c), bindings))
+
+            case _ => Add(left, right) // Default, for example add(x,y) with both unbound and no helpful distributivity anywhere
+        }
+    }
+
+    private def simplifyMul(left: Expression, right: Expression): Expression = {
+        (left, right) match {
+            case (Const(0), _) => Const(0)
+            case (_, Const(0)) => Const(0)
+            case (Const(1), r) => r
+            case (l, Const(1)) => l
+            case (Const(a), Const(b)) => Const(a * b)
+            case _ => Mul(left, right)
+        }
+    }
+
+    private def simplifySub(left: Expression, right: Expression): Expression = {
+        (left, right) match {
+            case (l, r) if l == r => Const(0)
+            case (Const(a), Const(b)) => Const(a - b)
+            case _ => Sub(left, right)
+        }
+    }
+
+    private def prettyPrint(expr: Expression): String = expr match {
+        case Const(i) => i.toString
+        case Var(name) => name
+        case Add(left, right) => s"${parenthesesCheck(left, "+")} + ${parenthesesCheck(right, "+")}"
+        case Mul(left, right) => s"${parenthesesCheck(left, "*")} * ${parenthesesCheck(right, "*")}"
+        case Sub(left, right) => s"${parenthesesCheck(left, "-")} - ${parenthesesCheck(right, "-")}"
+    }
+
+    private def parenthesesCheck(expr: Expression, parentOperator: String): String = {
+        val needsParentheses = expr match { // (l1 op2 l2) op (r1 op2 r2) --> if op2 < op, need parentheses
+            case Add(_, _) if precedence("+") < precedence(parentOperator) => true //I know that checking if parentOperator is * has the same effect...
+            case Sub(_, _) if precedence("-") < precedence(parentOperator) => true
+            case _ => false
+        }
+
+        val str = prettyPrint(expr)
+        if (needsParentheses) s"( $str )" else str
     }
 
     private def assign(variable: String, expr: Expression): String = {
-        // TODO: Implement variable assignment
-        s"$variable = assigned"
+        try {
+            val value = expr.value(currentBindings) //get the single constant value of our expression. Throws an error if there's an unbound variable
+            currentBindings = currentBindings + (variable -> value)
+            s"$variable = $value"
+        }
+        catch {
+            case e: NoSuchElementException =>
+                val missingVar = e.getMessage.replace("key not found: ", "")
+                s"Unknown variable: $missingVar"
+        }
     }
 
     private def evaluate(expr: Expression): String = {
-        // TODO: Implement evaluation
-        "evaluated result"
+        try {
+            val value = expr.value(currentBindings)
+            value.toString
+        }
+        catch {
+            case e: NoSuchElementException =>
+                val missingVar = e.getMessage.replace("key not found: ", "")
+                s"Unknown variable: $missingVar"
+        }
     }
 }
-
